8a8da0622835e10c543791e51efb1a4b76521a46
Janice Smith <janice.k.n.smith@gmail.com>
2020-04-08 16:44:00 -0400

    setting up place to notes and possible solutions to exercises

---
+++ fullstackory_adapter_pattern/section_d.rb
@@ -0,0 +1,49 @@
+#########################
+# require "fullstackory_adapter_pattern/version"
+# This is the complete a goal section
+# we lay out a problem and you slove it with the adaptor pattern
+# class SectionDAdapterPattern
+#   def intro
+#     intro_text = <<-HEREDOC
+#       Now you need to slove a problem with your own code!
+#     HEREDOC
+#     puts intro_text
+#   end
+# end
+
+# The goal here is to use the obscenity gem to santize text
+# More info about the gem here: https://github.com/tjackiw/obscenity
+# Write the code below as in the other sections
+# Be sure to write tests!
+
+# Hint: to add dependencies to a gem, you need to add them the the gemspec
+
+
+# The ___ class is the 'adaptee'
+
+
+# The ___ class is the 'Target'
+
+
+# The ___ is the 'adapter'
+#########################
+
+#Possible solution below
+
+class SectionDAdapterPattern
+  def intro
+    intro_text = <<-HEREDOC
+      Now you need to slove a problem with your own code!
+    HEREDOC
+    puts intro_text
+  end
+end
+
+# The ___ class is the 'adaptee'
+# TODO: put in possible solution
+
+# The ___ class is the 'Target'
+# TODO: put in possible solution
+
+# The ___ is the 'adapter'
+# TODO: put in possible solution 
